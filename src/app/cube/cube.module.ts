9c61a69a57059676c4769c0406898f33c107aaa2
Asha Owl <trishi-ri@ya.ru>
2020-10-11 12:23:04 +0300

    minor update (renaming)

--- src/app/cube/cube.module.ts
+++ src/app/cube/cube.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { CubeRoutingModule } from './cube-routing.module';
+import { CubeRoutingModule } from './cube.routing.module';
 import { CubeComponent } from './cube.component';
 import { CubeSideComponent } from './cube-side/cube-side.component';
 import { CubeControlsComponent } from './cube-controls/cube-controls.component';
